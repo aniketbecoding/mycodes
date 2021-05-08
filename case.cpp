88cfb85ae5dc5bbee4982de5d1208f904ec206e3
aniketbecoding <83643436+aniketbecoding@users.noreply.github.com>
2021-05-08 20:50:30 +0530

    Add files via upload

---
+++ case.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n1,n2;
+    cout<<"enter two numbers:";
+    cin>>n1>>n2;
+    char op;
+    cout<<"enter a character:"<<endl;
+    cin>>op;
+    switch(op)
+    {
+        case '+':
+        cout<<n1+n2<<endl;
+        break;
+        case '-':
+        cout<<n1-n2<<endl;
+        break;
+        default:
+        cout<<"enter another operator"<<endl;
+        break;
+    
+    }
+    return 0;
+}
