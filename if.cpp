d0e1743cc5a941df2d535be80938ed7546963d2b
aniketbecoding <83643436+aniketbecoding@users.noreply.github.com>
2021-05-08 21:15:45 +0530

    Add files via upload

---
+++ if.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the amount:"<<endl;
+    cin>>n;
+    if(n>500){
+        cout<<"cheese burger"<<endl;}
+    else{
+        cout<<"fries"<<endl;
+    }
+    
+    return 0;
+}
