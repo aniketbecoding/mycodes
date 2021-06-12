f510702ad749d365f5e1c4fa269400270cb4fb25
aniketbecoding <83643436+aniketbecoding@users.noreply.github.com>
2021-06-12 22:11:32 +0530

    Add files via upload

---
+++ maxfreq.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+int main(){
+string s;
+cin>>s;
+    
+    int freq[26];
+    for(int i=0;i<26;i++){
+        freq[i]=0;
+    }
+    for(int i=0;i<s.size();i++){
+        freq[s[i]-'a']++;
+    }
+    int maxf=0;
+    char ans='a';
+    for(int i=0;i<26;i++){
+        if(freq[i]>maxf){
+            maxf=freq[i];
+            ans=i+'a';
+        }
+
+    }
+    cout<<maxf<<" "<<ans<<endl;
+    return 0;
+}
