83e2a6298a9c811bd11b8c5185998288d589108b
aniketbecoding <83643436+aniketbecoding@users.noreply.github.com>
2021-06-17 21:18:16 +0530

    Add files via upload

---
+++ numofones.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int numofones(int n){
+    int count=0;
+    while(n){
+    n=n&(n-1);
+    count++;
+    }
+    return count;
+    
+
+}
+int main(){
+    cout<<numofones(8)<<endl;
+    return 0;
+}
