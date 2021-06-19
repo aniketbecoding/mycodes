cf0aedf81290f5e9cae0f2a9337198e807032c8f
aniketbecoding <83643436+aniketbecoding@users.noreply.github.com>
2021-06-19 21:13:42 +0530

    Add files via upload

---
+++ towerofhanoi.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void toh(int n, char src,char dest,char helper){
+    if(n==0){
+        return;
+    }
+    toh(n-1,src,helper,dest);
+    cout<<"MOVE FROM"<<src<<"TO"<<dest<<endl;
+    toh(n-1,helper,dest,src);
+}
+int main(){
+    toh(4,'A','C','B');
+    return 0;
+}
